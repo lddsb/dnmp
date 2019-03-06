178ac348a2a9930b855eb83b7322af462727f9f9
lddsb <luodi0128@gmail.com>
2019-03-06 14:59:52 +0800

    optimized nodejs image

--- nodejs/server.js
+++ nodejs/server.js
@@ -1,22 +1,10 @@
-/*
-* @Author: lddsb
-* @Date:   2018-09-25 15:06:00
-* @Last Modified by:   lddsb
-* @Last Modified time: 2018-09-25 15:06:15
-*/
 'use strict';
 
-const express = require('express');
-
-// Constants
-const PORT = 8080;
-const HOST = '0.0.0.0';
-
-// App
-const app = express();
-app.get('/', (req, res) => {
-  res.send('Hello world\n');
-});
-
-app.listen(PORT, HOST);
-console.log(`Running on http://${HOST}:${PORT}`);
+var http = require('http');
+var handleRequest = function (request, response) {
+  console.log('Received Request for URL: ' + request.url);
+  response.writeHead(200);
+  response.end('Welcome to Node world!');
+};
+var www = http.createServer(handleRequest);
+www.listen(8080);
